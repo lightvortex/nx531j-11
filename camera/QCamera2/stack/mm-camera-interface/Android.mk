e0e2994541f068967db3163b8f7ddadc7631254f
33bca <fabian.leutenegger@bluewin.ch>
2020-05-07 20:58:10 +0700

    nx531j: Adapt QCamera HAL pathmap for AOSPA
    Signed-off-by: sweetyicecare <gabrielseedev@outlook.com>

--- camera/QCamera2/stack/mm-camera-interface/Android.mk
+++ camera/QCamera2/stack/mm-camera-interface/Android.mk
@@ -39,7 +39,7 @@ LOCAL_COPY_HEADERS += ../common/cam_types.h
 LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/inc \
     $(LOCAL_PATH)/../common \
-    $(call project-path-for,qcom-media)/mm-core/inc \
+    hardware/qcom/media/mm-core/inc \
     system/media/camera/include \
 
 LOCAL_CFLAGS += -DCAMERA_ION_HEAP_ID=ION_IOMMU_HEAP_ID
