f97225557a5d134301af17d0b24efa49097e6bc7
lightvortex <rishishukla242@gmail.com>
2020-07-31 11:09:04 +0530

    Revert "nx531j: enable full dexpreopt"
    This reverts commit 023e1118d4c08ce732d1a180497899628f257ca1.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,15 +115,13 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
-# Enable dexpreopt to speed boot time
+# Dex
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-    endif
+    WITH_DEXPREOPT ?= true
   endif
 endif
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
 
 # Display
 MAX_VIRTUAL_DISPLAY_DIMENSION := 4096
