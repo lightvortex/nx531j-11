627e2b8a0dd16c7751d33a13fbbcd23e241068c5
LuK1337 <priv.luk@gmail.com>
2020-09-18 14:36:05 +0200

    nx531j: Build libhidltransport/libhwbinder
    * Needed for old blobs
    
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172

--- msm8996.mk
+++ msm8996.mk
@@ -251,7 +251,9 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0-java \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system
+    android.hidl.manager@1.0_system \
+    libhwbinder \
+    libhwbinder.vendor
 
 # IMS
 PRODUCT_PACKAGES += \
