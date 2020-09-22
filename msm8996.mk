bd07a608e58a8d757fcb7469e33f10fa5d7dcac0
Arian <arian.kulmer@web.de>
2020-09-22 20:06:14 +0200

    nx531j: Build new aidl qti power HAL
    Change-Id: I4bdea354e54d34d0dfe9b811e228fc6f8762d2ed

--- msm8996.mk
+++ msm8996.mk
@@ -248,11 +248,8 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
-    android.hidl.manager@1.0-java \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system \
+    libhidltransport \
+    libhidltransport.vendor \
     libhwbinder \
     libhwbinder.vendor
 
@@ -345,7 +342,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.nubia-libperfmgr
+    android.hardware.power-service-qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
