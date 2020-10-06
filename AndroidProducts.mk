c536acf1608498f764834e948b800fd37a570a1d
LightVortex <rishishukla242@gmail.com>
2020-10-06 13:43:23 +0530

    nx531j:havoc--->posp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,10 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_nx531j.mk
+    $(LOCAL_DIR)/potato_nx531j.mk
+    
+
+COMMON_LUNCH_CHOICES := \
+    potato_nx531j-user \
+    potato_nx531j-userdebug \
+    potato_nx531j-eng
