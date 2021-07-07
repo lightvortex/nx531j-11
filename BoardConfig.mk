b6d692fa4458c6aa4545137ceae4acadc5dd7661
lightvortex <rishishukla242@gmail.com>
2021-07-07 15:21:43 +0530

    nx531j:use temp sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -198,6 +198,7 @@ VENDOR_SECURITY_PATCH := 2018-10-01
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
+BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/temp
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # Wifi
